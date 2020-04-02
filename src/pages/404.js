629a12ee49e0bed761749753e3425dc67dd8644c
Matthew Amato <matt.amato@gmail.com>
2020-04-02 15:09:02 -0400

    Messing with pandas

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>Whoops!</h1>
+        <p>
+            You 404'd!
+        </p>
+    </Layout>
+)
